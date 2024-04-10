26f1526e722c86e272b4b84413d385d47f553976
Sander Hilven <sander.hilven@gmail.com>
2024-04-10 21:06:48 +0200

    removed duplicate communication services function

--- azure_func/resource_functions.py
+++ azure_func/resource_functions.py
@@ -330,21 +330,6 @@ def handle_cognitive_service(resource, rg, cognitive_client):
     except Exception as e:
         return {'Error': str(e)}
     
-def handle_communication_services(resource, rg, communication_client):
-    try:
-        print("Getting Communication Service...")
-        communication_services = communication_client.communication_service.list_by_resource_group(rg.name)
-        
-        for service in communication_services:
-            if service.name == resource.name:
-                communication_service_dict = service.as_dict()
-                return communication_service_dict
-
-        return {'Error': 'Communication Service not found'}
-
-    except Exception as e:
-        return {'Error': str(e)}
-    
 def handle_container_app(resource, rg, containerapp_client):
     try:
         # Get the Container App
